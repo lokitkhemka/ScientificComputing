245bafa33a90cdccc389e8a4738a449b45b90fd0
lokitkhemka <loukitkhemka@gmail.com>
2023-07-29 11:30:02 +0530

    Added check for square matrices in the function to compute determinant.

--- ComputeDeterminant.py
+++ ComputeDeterminant.py
@@ -9,20 +9,23 @@ This is an implementation of Algorithm 3.1 from  Gander, et al., Scientific Comp
 
 def DetLaplace(A):
     n = A.shape[1]
-    if n==1:
-        d = A[0,0]
-    else:
-        d= 0; v = 1
-        for j in range(n):
-            Temp = np.delete(A,0,0) #Removing the first row from the matrix
-            Temp = np.delete(Temp, j, 1) #Removing the jth column from resulting matrix
-            d += v * A[0,j] * DetLaplace(Temp)
-            v = -v
-    
-    return d
+    if (A.shape[0] == A.shape[1]):
+        if n==1:
+            d = A[0,0]
+        else:
+            d= 0; v = 1
+            for j in range(n):
+                Temp = np.delete(A,0,0) #Removing the first row from the matrix
+                Temp = np.delete(Temp, j, 1) #Removing the jth column from resulting matrix
+                d += v * A[0,j] * DetLaplace(Temp)
+                v = -v
+        
+        return d
+    return None
 
 
 
 
 
 print(DetLaplace(np.array(((1,0,0, 0),(0,2,0, 0),(0,0,2, 0),(0,0,0,1)))))
+print(DetLaplace(np.array(((1,0,0, 0),(0,2,0, 0),(0,0,2, 0)))))
